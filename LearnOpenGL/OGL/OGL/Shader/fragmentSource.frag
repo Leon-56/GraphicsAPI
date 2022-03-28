3e27cbaa9543ee3256140db5d1fd772a8cc9878d
Leon-56 <963419094@qq.com>
2022-03-28 23:36:03 +0800

    [OGL] Add different light casters.

--- LearnOpenGL/OGL/OGL/Shader/fragmentSource.frag
+++ LearnOpenGL/OGL/OGL/Shader/fragmentSource.frag
@@ -6,10 +6,18 @@ struct Material {
 };
 struct Light {
     vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    // parms for point light caculate
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 uniform Light light;
@@ -26,11 +34,18 @@ uniform vec3 viewPos;
 
 void main()
 {
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
-    
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    
+    vec3 lightDir = normalize(light.position - FragPos);
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    // 执行光照计算
+    float distance    = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+
     float diff = max(dot(norm, lightDir), 0.0f);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
 
@@ -39,6 +54,14 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
 
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    diffuse  *= intensity;
+    specular *= intensity;
+
     vec3 result = (ambient + diffuse + specular);
     FragColor = vec4(result, 1.0);
+
 }
--- LearnOpenGL/OGL/OGL/Source/main.cpp
+++ LearnOpenGL/OGL/OGL/Source/main.cpp
@@ -56,16 +56,16 @@ float vertices[] = {
 };
 
 glm::vec3 cubePositions[] = {
-	glm::vec3( 0.0f,  0.0f,  0.0f), 
-    glm::vec3( 2.0f,  5.0f, -15.0f), 
-    glm::vec3(-1.5f, -2.2f, -2.5f),  
-    glm::vec3(-3.8f, -2.0f, -12.3f),  
-    glm::vec3( 2.4f, -0.4f, -3.5f),  
-    glm::vec3(-1.7f,  3.0f, -7.5f),  
-    glm::vec3( 1.3f, -2.0f, -2.5f),  
-    glm::vec3( 1.5f,  2.0f, -2.5f), 
-    glm::vec3( 1.5f,  0.2f, -1.5f), 
-    glm::vec3(-1.3f,  1.0f, -1.5f)  
+	glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 unsigned int indices[] = {  
@@ -186,13 +186,20 @@ int main()
 
 		// draw cube.
 		myShader->use();
-		myShader->setVec3("lightPos", lightPos);
+		myShader->setVec3("light.position", lightPos);
 		myShader->setVec3("viewPos", camera.Position);
+		myShader->setVec3("light.direction", camera.Front);
+		myShader->setFloat("light.cutOff",   glm::cos(glm::radians(12.5f)));
+		myShader->setFloat("light.outerCutOff",   glm::cos(glm::radians(17.5f)));
 
 		// set light.
 		myShader->setVec3("light.ambient",  glm::vec3(0.2f, 0.2f, 0.2f));
 		myShader->setVec3("light.diffuse",  glm::vec3(0.5f, 0.5f, 0.5f)); // 将光照调暗了一些以搭配场景
 		myShader->setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+		myShader->setFloat("light.constant",  1.0f);
+		myShader->setFloat("light.linear",    0.09f);
+		myShader->setFloat("light.quadratic", 0.032f);
 
 		// set material.
 		myShader->setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
@@ -204,7 +211,6 @@ int main()
 
 		myShader->setMat4("view", view);
 		myShader->setMat4("projection", projection);
-		myShader->setMat4("model", model);
 
 		// bind textures on corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
@@ -213,11 +219,20 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, specularMap);
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for(unsigned int i = 0; i < 10; i++)
+		{
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			myShader->setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// draw light source.
 		lightShader->use();
-		model = glm::mat4();
+		model = glm::mat4(1.0f);
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f));
 		
